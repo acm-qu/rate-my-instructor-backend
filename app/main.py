8adc58e5f74ab222241e0eb014075792c35938c8
oelmokadem <omarsamirelmokadem@gmail.com>
2026-05-27 01:41:07 +0300

    feat: add Alembic configuration and initial migration for database schema

--- app/main.py
+++ app/main.py
@@ -1,13 +1,4 @@
-from db.session import engine, Base
-from db import models
 from fastapi import FastAPI
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    init_db()
-    print("Tables created")
 
 app = FastAPI()
 
