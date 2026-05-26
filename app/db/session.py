f0b17c79ed042046f4b94067f09be66c9d46b266
oelmokadem <omarsamirelmokadem@gmail.com>
2026-05-26 18:21:46 +0300

    feat: add database session management with context generator for FastAPI

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from typing import Generator
 from dotenv import load_dotenv
 import os
+
 
 load_dotenv()
 
@@ -23,3 +25,10 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+def get_db() -> Generator:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
